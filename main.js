328ab87cf2d99d6b9b4f41d1469aba27eb05cf94
gurnisha-rehal <gkr11@sfu.ca>
2026-02-01 00:01:55 -0800

    Created javascript pages

---
+++ main.js
@@ -0,0 +1,18 @@
+//DOM selection
+
+//Event listeners
+
+//Conditional logic
+
+const contactBtn = document.getElementById("contactBtn");
+const contactInfo = document.getElementById("contactInfo");
+
+contactBtn.addEventListener("click", () => {
+  if (contactInfo.style.display === "none") {
+    contactInfo.style.display = "block";
+    contactBtn.textContent = "Hide Contact Info";
+  } else {
+    contactInfo.style.display = "none";
+    contactBtn.textContent = "Show Contact Info";
+  }
+});
