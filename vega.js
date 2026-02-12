a636ecd9b4c3f56a6cec4741cc2f50fb7c65fb88
gurnisha-rehal <gkr11@sfu.ca>
2026-02-11 16:07:22 -0800

    Visualization 1

--- vega.js
+++ vega.js
@@ -8,12 +8,27 @@ async function fetchData(){
 
 fetchData().then(async (data) => {
   const vlSpec = vl
-  .markBar()
+  .markCircle({tooltip: true})
   .data(data)
+  .transform(
+    vl.aggregate([
+      { op: "sum", field: "Global_Sales", as: "TotalSales" }
+    ]).groupby(["Platform", "Genre"]),
+
+    // Step 2: Rank genres within each platform
+    vl.window([
+      { op: "rank", as: "rank" }
+    ])
+    .sort([{ field: "TotalSales", order: "descending" }])
+    .groupby(["Platform"]),
+
+    // Step 3: Keep only top genre per platform
+    vl.filter("datum.rank == 1")
+  )
   .encode(
   vl.y().fieldN("Platform").sort("-x"),
-  vl.x().fieldQ("Global_Sales").aggregate("sum"),
-  vl.color().fieldN("Genre")
+  vl.x().fieldQ("TotalSales").title("Highest Selling Genre"),
+  vl.color().fieldN("Genre").title("Top Genre")
 )
 .width("container")
 .height(400)
