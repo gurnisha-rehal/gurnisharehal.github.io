f0b0919acdae8a4431ef8df7e6c69e190f064a6e
gurnisha-rehal <gkr11@sfu.ca>
2026-02-12 22:23:51 -0800

    Vis 5/6

--- vega.js
+++ vega.js
@@ -1,15 +1,17 @@
 //Vega-lite Visualizations
   //Load data from dataset/videogames_wide.csv using d3.csv and then make visualizations
 
-async function fetchData(){
-  const data = await d3.csv("./dataset/videogames_wide.csv");
-  return data;
+async function fetchData() {
+  const wide = await d3.csv("./dataset/videogames_wide.csv");
+  const long = await d3.csv("./dataset/videogames_long.csv");
+
+  return { wide, long };
 }
 
-fetchData().then(async (data) => {
+fetchData().then(async ({ wide, long }) => {
   const vlSpec = vl
   .markCircle({tooltip: true})
-  .data(data)
+  .data(wide)
   .transform(
     vl.aggregate([
       { op: "sum", field: "Global_Sales", as: "TotalSales" }
@@ -39,7 +41,7 @@ fetchData().then(async (data) => {
 
   const vlSpec2 = vl
   .markCircle({tooltip: true})
-  .data(data)
+  .data(wide)
   .transform(
     vl.aggregate([
         { op: "distinct", field: "Genre", as: "GenreCount" },
@@ -67,7 +69,7 @@ fetchData().then(async (data) => {
 
   const vlSpec3 = vl
   .markLine({point: true, tooltip: true})
-  .data(data)
+  .data(wide)
 
   //Remove N/A years
   .transform(
@@ -90,7 +92,7 @@ fetchData().then(async (data) => {
 
   const vlSpec4 = vl
   .markBar({ tooltip: true })
-  .data(data)
+  .data(wide)
   .transform(
 
     vl.filter("datum.Year != null"),
@@ -115,27 +117,102 @@ fetchData().then(async (data) => {
     vl.y().fieldQ("TotalSales")
       .title("Total Global Sales"),
 
-    vl.color().fieldN("Publisher"),
-
-    vl.tooltip([
-      vl.fieldN("Publisher"),
-      vl.fieldO("Year"),
-      vl.fieldQ("TotalSales")
-    ])
+    vl.color().fieldN("Publisher")
   )
   .width(700)
   .height(400)
   .toSpec();
 
+   const vlSpec5 = vl
+  .markBar({ tooltip: true })
+  .data(long)
+  .transform(
+    // Aggregate total sales per platform + region
+    vl.aggregate([
+      { op: "sum", field: "sales_amount", as: "TotalSales" }
+    ]).groupby(["platform", "sales_region"])
+  )
+  .encode(
+    // Platforms on Y-axis
+    vl.y()
+      .fieldN("platform")
+      .sort({ op: "sum", field: "sales_amount"})
+      .title("Platform"),
+
+    // Sales on X-axis (stacked)
+    vl.x()
+      .fieldQ("TotalSales")
+      .stack("zero")
+      .title("Total Sales (Millions)"),
+
+    // Color legend for regions
+    vl.color()
+      .fieldN("sales_region")
+      .title("Region"),
+  )
+  .width(700)
+  .height(500)
+  .toSpec();
+
+  const vlSpec6 = vl
+  .markCircle({ opacity: 0.7, tooltip: true })
+  .data(long)
+  .transform(
+    // Aggregate total sales per platform + region
+    vl.aggregate([
+      { op: "sum", field: "sales_amount", as: "TotalSales" }
+    ]).groupby(["platform", "sales_region"])
+  )
+  .encode(
+    vl.y()
+      .fieldN("sales_region")
+      .title("Region"),
+
+    vl.x()
+      .fieldN("platform")
+      .sort("-size")
+      .title("Platform"),
+
+    vl.size()
+      .fieldQ("TotalSales")
+      .scale({ range: [0, 1000] })
+      .title("Total Sales"),
+
+    vl.color()
+      .fieldN("sales_region")
+      .legend(null), 
+
+  )
+  .width(700)
+  .height(500)
+  .toSpec();
 
 
-
+  const vlSpec7 = vl
+  .markBar({ tooltip: true })
+  .data(long)
+  .transform(
+    // Aggregate total sales per platform + region
+    vl.aggregate([
+      { op: "sum", field: "sales_amount", as: "TotalSales" }
+    ]).groupby(["platform", "sales_region"])
+  )
+  .encode(
+    vl.x().fieldN("sales_region").title("Region"),
+    vl.y().fieldQ("TotalSales").title("Total Sales (Millions)"),
+  )
+  .width(700)
+  .height(500)
+  .toSpec();
 
 
 render("#view", vlSpec);
 render("#view2", vlSpec2);
 render("#view3", vlSpec3);
 render("#view4", vlSpec4);
+render("#view5", vlSpec5);
+render("#view6", vlSpec6);
+render("#view6", vlSpec7);
 });
 
 async function render(viewID, spec) {
